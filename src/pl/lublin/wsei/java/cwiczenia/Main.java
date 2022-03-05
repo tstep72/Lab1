bbc8b78f64e60bc3ed57b6c597902632fa759de3
Tomasz Stepien <tomasz.steppien@gmail.com>
2022-03-05 11:40:50 +0100

    Lab1, krok 3 i 4 - komentarze i wypisanie na ekranie

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,21 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+/** komentarz javadoc, zawierający dokumentację opisującą działanie klasy/metod
+ *
+ * @param  parametr  parametr metody, nie występuje
+ * @return           co zwraca metoda
+ * @see    Main
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        //komentarz jednolinijkowy
+
+        /* komentarz
+          wielo-
+          linijkowy
+          */
+        System.out.println("Hello Java world!\n");
+    }
+}
